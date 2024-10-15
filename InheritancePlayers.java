b572e2c652a1d4a1ff9ad22f7b1315d7c0c9f993
Heidi <h.tang19@theabbey.co.uk>
2024-10-15 08:23:14 +0000

    Co-authored-by: dioncheungdion <dioncheungdion@users.noreply.github.com> Co-authored-by: yzh0502 <yzh0502@users.noreply.github.com> Co-authored-by: olive ^_^ <olive-v@users.noreply.github.com>

--- InheritancePlayers.java
+++ InheritancePlayers.java
@@ -2,7 +2,7 @@ public class InheritancePlayers {
 
     public static void main(String[] args){
 
-        Player1 player1 = new Player1("dion", 'x');
+        Player1 player1 = new Player1(String newName, String newSiund);
         player1.displayInfo();
 
         Player2 player2 = new Player2("olive", 'o');
--- Player.java
+++ Player.java
@@ -1,23 +1,7 @@
 public class Player {
 
-        Player player1 = new Player('')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //public String name;
-    //public char symbol;
+        public String name;
+        public char symbol;
 
     //public Players(String name, char symbol){
        // this.name = name;
