18a2f80b9eb8d83ba335d4c65b2bcc56ca7d187c
Heidi <h.tang19@theabbey.co.uk>
2024-10-15 08:54:19 +0000

    commit

--- Board.java
+++ Board.java
@@ -14,7 +14,7 @@ public class Board {
 
     public void printBoard() {
         for (int i = 0;i<3;i++){
-            System.out.print(strArr[i][0]+" "+strArr[i][1]+" "+strArr[i][2]);
+            System.out.print(strArr[i][0]+" "+strArr[i][1]+" "+strArr[i][2]+\n);
         }
     }
 
